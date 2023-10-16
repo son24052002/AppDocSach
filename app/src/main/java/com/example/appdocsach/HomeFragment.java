d96b3daca1193bbb793de2713d97761d3910aace
nhantuan22 <145580162+nhantuan22@users.noreply.github.com>
2023-10-16 16:15:30 +0700

    tao navigation menu

--- app/src/main/java/com/example/appdocsach/HomeFragment.java
+++ app/src/main/java/com/example/appdocsach/HomeFragment.java
@@ -2,16 +2,24 @@ package com.example.appdocsach;
 
 import android.os.Bundle;
 
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 
 public class HomeFragment extends Fragment {
+
 
 
 
@@ -29,10 +37,40 @@ public class HomeFragment extends Fragment {
         // Kết nối TabLayout và ViewPager
         tabLayout.setupWithViewPager(viewPager);
 
+
+        //navigation
+
+        Toolbar toolbar = myview.findViewById(R.id.toolbar);
+        DrawerLayout drawerLayout = myview.findViewById(R.id.drawerLayout);
+        NavigationView navigationView = myview.findViewById(R.id.navigationView);
+        ListView listView = myview.findViewById(R.id.listview);
+
+        //actionToolBar();
+
+        toolbar.setNavigationIcon(R.drawable.baseline_menu);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+
         return myview;
 
 
     }
+
+//    private void actionToolBar() {
+//        toolbar
+//        toolbar.setNavigationIcon(R.drawable.baseline_menu);
+//        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                drawerLayout.openDrawer(GravityCompat.START);
+//            }
+//        });
+//    }
 
 
 }
