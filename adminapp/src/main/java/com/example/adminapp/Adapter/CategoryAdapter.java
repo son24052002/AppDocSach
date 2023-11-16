0b73423b7146ad6857ea5126994301e1b932929e
son24052002 <son240502@gmail.com>
2023-11-16 22:47:55 +0700

    them add img cho trang addbook

--- adminapp/src/main/java/com/example/adminapp/Adapter/CategoryAdapter.java
+++ adminapp/src/main/java/com/example/adminapp/Adapter/CategoryAdapter.java
@@ -1,6 +1,7 @@
 package com.example.adminapp.Adapter;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,10 +11,15 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.adminapp.Model.Category;
 import com.example.adminapp.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
@@ -48,10 +54,49 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Holder
         holder.btnDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(context, ""+CategoryName,Toast.LENGTH_SHORT).show();
+                //Toast.makeText(context, ""+CategoryName,Toast.LENGTH_SHORT).show();
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle("Delete")
+                        .setMessage("Bạn chắc chắn muốn xóa?")
+                        .setPositiveButton("Chắc chắn", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(context,"Đang xóa...", Toast.LENGTH_SHORT).show();
+                                deleteCategory(model,holder);
+
+                            }
+                        })
+                        .setNegativeButton("Hủy", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        })
+                        .show();
             }
         });
 
+    }
+
+    private void deleteCategory(Category model, HolderCategory holder){
+        //lay id cua category cần xóa
+        String id = model.getId();
+        //categories > categoryId
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Categories");
+        databaseReference.child(id)
+                .removeValue()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(context, "Xóa thành công!!!", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(context, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     @Override
--- adminapp/src/main/java/com/example/adminapp/PdfAddActivity.java
+++ adminapp/src/main/java/com/example/adminapp/PdfAddActivity.java
@@ -16,9 +16,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.adminapp.Model.Book;
 import com.example.adminapp.Model.Category;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -32,8 +34,11 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Locale;
 
 public class PdfAddActivity extends AppCompatActivity {
 
@@ -44,10 +49,12 @@ public class PdfAddActivity extends AppCompatActivity {
 
     //uri pdf
     private Uri pdfUri = null;
-
-    private ArrayList<Category> categoryArrayList;
+    private Uri imgUri = null;
+    ImageView imgBook;
+    private ArrayList<String> categoryTitleArrayList, categoryIdArrayList;
 
     private ProgressDialog progressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +69,7 @@ public class PdfAddActivity extends AppCompatActivity {
         progressDialog.setTitle("Vui lòng đợi...");
         progressDialog.setCanceledOnTouchOutside(false);
 
+        imgBook = findViewById(R.id.imgBook);
 
         //nut tro lai
         ImageButton btnBack = findViewById(R.id.btnBack);
@@ -98,9 +106,26 @@ public class PdfAddActivity extends AppCompatActivity {
                 validateData();
             }
         });
+
+        //chon img
+        Button btnSelectImg = findViewById(R.id.btnSelectImg);
+
+        btnSelectImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImg();
+            }
+        });
     }
 
-    private String tieude="", mota = "", theloai ="";
+    private void selectImg() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,100);
+    }
+
+    private String tieude="", mota = "";
     private void validateData() {
         EditText edtTitle = findViewById(R.id.edtTitle);
         EditText edtDescription = findViewById(R.id.edtDescription);
@@ -108,22 +133,56 @@ public class PdfAddActivity extends AppCompatActivity {
 
         tieude = edtTitle.getText().toString().trim();
         mota = edtDescription.getText().toString().trim();
-        theloai = tvCategory.getText().toString().trim();
+
 
         if(TextUtils.isEmpty(tieude)){
             Toast.makeText(this, "Nhập tiêu đề sách...", Toast.LENGTH_SHORT).show();
         } else if (TextUtils.isEmpty(mota)) {
             Toast.makeText(this, "Nhập mô tả sách...", Toast.LENGTH_SHORT).show();
-        } else if (TextUtils.isEmpty(theloai)) {
+        } else if (TextUtils.isEmpty(selectedCategoryName)) {
             Toast.makeText(this, "Chọn thể loại sách...", Toast.LENGTH_SHORT).show();
         } else if (pdfUri == null) {
             Toast.makeText(this, "Chọn Pdf...", Toast.LENGTH_SHORT).show();
+        } else if (imgUri == null) {
+            Toast.makeText(this, "Chọn ảnh...",Toast.LENGTH_SHORT).show();
         } else {
-            uploadPdfToStorage();
+            uploadPdfToStorageAndImage();
+
         }
     }
 
-    private void uploadPdfToStorage() {
+    public void uploadImageToStorage(String uploadedPdfUrl, long timestamp, Uri imgUri) {
+
+        //path cua img tren firebase storage
+        SimpleDateFormat format = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.ENGLISH);
+        Date dateNow = new Date();
+        String fileName = format.format(dateNow);
+
+
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference("images/"+fileName);
+        storageReference.putFile(imgUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        //get url imgbook
+                        Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
+                        while (!uriTask.isSuccessful());
+                        String uploadedImgBookUrl = ""+uriTask.getResult();
+
+                        //upload to firebase db
+                        uploadToDatabase(uploadedImgBookUrl, uploadedPdfUrl, timestamp);
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(PdfAddActivity.this,""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void uploadPdfToStorageAndImage() {
         //show progress dialog
         progressDialog.setMessage("Đang tải lên...");
         progressDialog.show();
@@ -131,11 +190,18 @@ public class PdfAddActivity extends AppCompatActivity {
         //timestamp
         long timestamp = System.currentTimeMillis();
 
+        //path cua img tren firebase storage
+        SimpleDateFormat format = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.ENGLISH);
+        Date dateNow = new Date();
+        String fileName = format.format(dateNow);
+
+
+        StorageReference imgStorageReference = FirebaseStorage.getInstance().getReference("images/"+fileName);
+
         //path of pdf on firebase storage
         String filePathAndName = "Books/"+timestamp;
-
-        StorageReference storageReference = FirebaseStorage.getInstance().getReference(filePathAndName);
-        storageReference.putFile(pdfUri)
+        StorageReference pdfStorageReference = FirebaseStorage.getInstance().getReference(filePathAndName);
+        pdfStorageReference.putFile(pdfUri)
                 .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                     @Override
                     public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
@@ -146,8 +212,8 @@ public class PdfAddActivity extends AppCompatActivity {
                         while (!uriTask.isSuccessful());
                         String uploadedPdfUrl = ""+uriTask.getResult();
 
-                        //upload to firebase db
-                        uploadPdfToDB(uploadedPdfUrl, timestamp);
+
+                        uploadImageToStorage(uploadedPdfUrl, timestamp, imgUri);
 
                     }
                 })
@@ -161,14 +227,17 @@ public class PdfAddActivity extends AppCompatActivity {
                 });
     }
 
-    private void uploadPdfToDB(String uploadedPdfUrl, long timestamp) {
+    private void uploadToDatabase(String uploadedImgUrl, String uploadedPdfUrl,  long timestamp) {
         HashMap<String, Object> hashMap = new HashMap<>();
         hashMap.put("id",""+timestamp);
         hashMap.put("BookTitle", ""+tieude);
         hashMap.put("BookDescription", ""+mota);
-        hashMap.put("Category",""+theloai);
+        hashMap.put("CategoryId",""+selectedCategoryId);
         hashMap.put("url",""+uploadedPdfUrl);
+        hashMap.put("imgUrl",""+uploadedImgUrl);
         hashMap.put("timestamp",timestamp);
+
+
 
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Books");
         databaseReference.child(""+timestamp)
@@ -190,19 +259,22 @@ public class PdfAddActivity extends AppCompatActivity {
     }
 
     private void loadCategories() {
-        categoryArrayList = new ArrayList<>();
-
+        categoryTitleArrayList = new ArrayList<>();
+        categoryIdArrayList = new ArrayList<>();
         //load data tu firebase
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Categories");
         databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                categoryArrayList.clear(); //xoa du lieu da add vao tu truoc
+                categoryTitleArrayList.clear(); //xoa du lieu da add vao tu truoc
+                categoryIdArrayList.clear();
                 for(DataSnapshot dataSnapshot: snapshot.getChildren()){
-                    //getDAta
-                    Category model = dataSnapshot.getValue(Category.class);
-                    //add to arraylist
-                    categoryArrayList.add(model);
+                    //lay id va ten của category
+                    String categoryId = ""+dataSnapshot.child("id").getValue();
+                    String categoryName = ""+dataSnapshot.child("categoryName").getValue();
+
+                    categoryIdArrayList.add(categoryId);
+                    categoryTitleArrayList.add(categoryName);
                 }
             }
 
@@ -213,10 +285,11 @@ public class PdfAddActivity extends AppCompatActivity {
         });
     }
 
+    private String selectedCategoryId, selectedCategoryName;
     private void categoryPickDialog() {
-        String[] categoriesArray = new String[categoryArrayList.size()];
-        for(int i=0; i < categoryArrayList.size(); i++){
-            categoriesArray[i] = categoryArrayList.get(i).getCategoryName();
+        String[] categoriesArray = new String[categoryTitleArrayList.size()];
+        for(int i = 0; i < categoryTitleArrayList.size(); i++){
+            categoriesArray[i] = categoryTitleArrayList.get(i);
         }
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -224,10 +297,11 @@ public class PdfAddActivity extends AppCompatActivity {
                 .setItems(categoriesArray, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        String category = categoriesArray[which];
+                        selectedCategoryName = categoryTitleArrayList.get(which);
+                        selectedCategoryId = categoryIdArrayList.get(which);
 
                         TextView tvCat = findViewById(R.id.tvCat);
-                        tvCat.setText(category);
+                        tvCat.setText(selectedCategoryName);
                     }
                 })
                 .show();
@@ -254,9 +328,17 @@ public class PdfAddActivity extends AppCompatActivity {
                 Log.d(TAG, "onActivityResult: URI: "+pdfUri);
             }
         }
-        else{
 
-            Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+        if(requestCode == 100 && resultCode == RESULT_OK){
+            if(data != null){
+                imgUri = data.getData();
+                imgBook.setImageURI(imgUri);
+            }
+            else {
+                // Xử lý khi không có dữ liệu trả về từ việc chọn ảnh
+                Toast.makeText(this, "Không có dữ liệu ảnh được chọn", Toast.LENGTH_SHORT).show();
+            }
         }
+
     }
 }
